563970bdab514f0675f8decdc2c5d96ed04a82fd
Hi-Im-Slag <105272655+Hi-Im-Slag@users.noreply.github.com>
2023-09-10 13:25:57 +0700

    Sass component

---
+++ capstone/src/pages/TempComponent.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const TempComponent = () => {
+  return (
+    <>
+      <div>tempComponent</div>
+      <div>Button</div>
+      <div>Icon button</div>
+      <div>Search bar</div>
+      <div>Dropdown</div>
+      <div>Tab bar</div>
+      <div>Mini kit</div>
+      <div>Chips</div>
+      <div>Avatar</div>
+      <div>Toast</div>
+      <div>Cards</div>
+    </>
+  );
+};
+
+export default TempComponent;
