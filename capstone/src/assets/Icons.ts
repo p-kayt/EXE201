27cf325f6510a7c454afe76211fc26aa7b7b3a05
Kayt <trieuphamkt59@gmail.com>
2023-10-07 22:48:49 +0700

    reimport icon

--- capstone/src/assets/Icons.ts
+++ capstone/src/assets/Icons.ts
@@ -1,20 +1,20 @@
-import Arrow from "../../assets/icons/arrow.svg";
-import Check from "../../assets/icons/check.svg";
-import SearchIcon from "../../assets/icons/search-linear.svg";
-import Flame from "../../assets/icons/Flame.svg";
-import Logo from "../../assets/icons/logo.svg";
-import Sms from "../../assets/icons/sms.svg";
-import Facebook from "../../assets/icons/facebook.svg";
-import Instagram from "../../assets/icons/instagram.svg";
-import Twitch from "../../assets/icons/twitch.svg";
-import Youtube from "../../assets/icons/youtube.svg";
-import Share from "../../assets/icons/share.svg";
-import Notification from "../../assets/icons/notification.svg";
-import Messenger from "../../assets/icons/messenger.svg";
-import AddCircleBold from "../assets/icons/add-circle-bold.svg";
-// import searchIcon from "../assets/icons/search-linear.svg";
-import Eye from "../../assets/icons/eye.svg";
-import EyeSlash from "../../assets/icons/eye-slash.svg";
+import Arrow from "./icons/arrow.svg";
+import Check from "./icons/check.svg";
+import SearchIcon from "./icons/search-linear.svg";
+import Flame from "./icons/Flame.svg";
+import Logo from "./icons/logo.svg";
+import Sms from "./icons/sms.svg";
+import Facebook from "./icons/facebook.svg";
+import Instagram from "./icons/instagram.svg";
+import Twitch from "./icons/twitch.svg";
+import Youtube from "./icons/youtube.svg";
+import Share from "./icons/share.svg";
+import Notification from "./icons/notification.svg";
+import Messenger from "./icons/messenger.svg";
+import AddCircleBold from "./icons/add-circle-bold.svg";
+// import searchIcon from "icons/search-linear.svg";
+import Eye from "./icons/eye.svg";
+import EyeSlash from "./icons/eye-slash.svg";
 
 export {
   Logo,
