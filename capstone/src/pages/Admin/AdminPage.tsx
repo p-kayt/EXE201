4c816c2d786af13dc49a34658a7b33f435fa058c
Hi-Im-Slag <105272655+Hi-Im-Slag@users.noreply.github.com>
2023-11-01 17:39:22 +0700

    AdminPage ongoing

---
+++ capstone/src/pages/Admin/AdminPage.tsx
@@ -0,0 +1,32 @@
+import React, { useEffect } from "react";
+import "./userProfile.scss";
+import { Outlet, useParams } from "react-router-dom";
+import { useDispatch } from "react-redux";
+
+import { getUser } from "../../store/api-thunk/userThunk";
+import { ThunkDispatch } from "@reduxjs/toolkit";
+import AdminNav from "./AdminNav";
+type Props = {};
+
+const AdminPage = (props: Props) => {
+  const dispatch = useDispatch<ThunkDispatch<any, any, any>>();
+  let { ID } = useParams();
+
+  useEffect(() => {
+    dispatch(getUser({ ID }));
+  }, [ID]);
+  return (
+    <>
+      <div className="profile-container">
+        <div className="profile-nav">
+          <AdminNav />
+        </div>
+        <div className="profile-content">
+          <Outlet />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default AdminPage;
