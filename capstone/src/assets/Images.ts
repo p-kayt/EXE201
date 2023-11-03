0783aa5a6164ac58e22b4b0d37322fa6173cc0d6
Kayt <trieuphamkt59@gmail.com>
2023-11-03 13:22:44 +0700

    update default course image

--- capstone/src/assets/Images.ts
+++ capstone/src/assets/Images.ts
@@ -22,7 +22,7 @@ export { default as QR } from "./images/qrimg.jpg";
 export { default as Napas } from "./images/Napas247.b58ff17b.png";
 export { default as VietQR } from "./images/VietQR.46a78cbb.png";
 export { default as CourseImg } from "./images/CourseImg.png";
-
+export { default as CourseThumbnail } from "./images/default-thumbnail.jpg";
 export { default as rating0 } from "./images/rate0.png";
 export { default as rating1 } from "./images/rate1.png";
 export { default as rating2 } from "./images/rate2.png";
